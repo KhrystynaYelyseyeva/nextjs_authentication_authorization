99e2e9a29ccf1207bd912fa0f53d4eb35c914d4b
Khrystyna Yelyseyeva <play1409@gmail.com>
2025-08-13 19:43:23 +0300

    make route compatible with Next.js's expectations for API route handlers

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,5 +1,3 @@
-// app/api/auth/me/route.ts
-
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { verifyToken, generateAccessToken } from "@/lib/auth-utils";
